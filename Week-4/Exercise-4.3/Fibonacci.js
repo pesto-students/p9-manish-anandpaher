8c37c6446a165fb951fc1280f9f5ae58d6634a05
Anand Aher <46132450+anandpaher@users.noreply.github.com>
2023-02-11 23:05:09 +0530

    Initial Commit

---
+++ Week-4/Exercise-4.3/Fibonacci.js
@@ -0,0 +1,19 @@
+const Fib = (num) => ({
+  [Symbol.iterator]: () => {
+    let i = 1,
+      old = 0,
+      new1 = 0;
+    return {
+      next: () => {
+        if (i++ <= num) {
+          [old, new1] = [new1, old + new1 || 1];
+          return { value: old, done: false };
+        } else {
+          return { done: true };
+        }
+      },
+    };
+  },
+});
+
+console.log("The Fibonacci Series is: " + [...Fib(7)]);
